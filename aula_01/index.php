bf4251eabd4319adccb5ae6684c28e9cd6377540
Julia Granado <79951728+gferri-projects@users.noreply.github.com>
2022-02-17 09:01:21 -0300

    Aula 17/02

---
+++ aula_01/index.php
@@ -0,0 +1,25 @@
+<?php
+    //comentario
+
+    /*comentario
+    comentario
+    comentario
+    comentario
+    */
+
+    $nome = "Julia";
+    $idade = 16;
+    $trabalhando = true;
+    $numero = "12";
+
+    echo "Meu nome é $nome";
+    echo "<br>";
+    echo $idade;
+    echo "<br>";
+    echo $trabalhando;
+    echo "<br>";
+    echo $idade . $numero;
+    echo "<br>";
+    var_dump($trabalhando);
+
+?>
